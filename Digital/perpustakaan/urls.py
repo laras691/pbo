132ad164aec2288480bd83894b820d76bcd0afc5
sya <arsyanddanareswari.05@email.com>
2025-05-31 21:06:29 +0700

    rev

--- Digital/perpustakaan/urls.py
+++ Digital/perpustakaan/urls.py
@@ -8,6 +8,8 @@ from .views import dashboard_admin
 from .views import AdminLoginView
 from django.views.generic import TemplateView
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.views import LoginView
+from django.contrib import admin
 
 urlpatterns = [
     path('', lambda request: redirect('login_pengunjung'), name='root_redirect'),
@@ -33,10 +35,10 @@ urlpatterns = [
 
     path('verifikasi/', views.verifikasi_kode, name='verifikasi_kode'),
 
-    path('admin-custom/login/', admin_custom_login, name='admin-custom-login'),
     path('admin-custom/', views.admin_custom, name='admin_custom'),
-    path('admin/login/', AdminLoginView.as_view(), name='admin-login'),
-    path('admin/dashboard/', staff_member_required(
+    path('admin/', admin.site.urls),  
+    path('custom-admin/login/', LoginView.as_view(template_name='admin/login.html'), name='admin-login'),
+    path('custom-admin/dashboard/', staff_member_required(
         TemplateView.as_view(template_name='admin/dashboard.html')
     ), name='admin-dashboard'),
     path('cari-buku/', views.cari_buku, name='cari_buku'),
